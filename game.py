ac71a6ec21254765238699319a5bff9672ec724c
Anita Nandi <anitan@ndph.ox.ac.uk>
2020-02-09 15:37:06 +0000

    Create Game class

---
+++ game.py
@@ -0,0 +1,42 @@
+from random import randint
+
+from board import Board
+from colour import Colour
+
+
+class Game:
+    def __init__(self):
+        self.board = Board.create_starting_board()
+        self.first_player = Colour(randint(0, 1))
+
+    def run_game(self, verbose=True):
+        if verbose:
+            print('%s goes first' % self.first_player)
+            self.board.print_board()
+        i = self.first_player.value
+        while True:
+            dice_roll = [randint(1, 6), randint(1, 6)]
+            if dice_roll[0] == dice_roll[1]:
+                dice_roll = [dice_roll[0]] * 4
+            colour = Colour(i % 2)
+            if verbose:
+                print("%s rolled %s" % (colour, dice_roll))
+            self.make_move(dice_roll, colour)
+            if verbose:
+                self.board.print_board()
+            i = i + 1
+            if self.board.has_game_ended():
+                if verbose:
+                    print('%s has won!' % self.board.who_won())
+                break
+
+    def make_move(self, dice_roll, colour):
+        for die_roll in dice_roll:
+            valid_pieces = self.board.get_pieces(colour)
+            for piece in valid_pieces:
+                if self.board.is_move_possible(piece, die_roll):
+                    self.board.move_piece(piece, die_roll)
+                    break
+
+    def who_won(self):
+        return self.board.who_won()
--- main.py
+++ main.py
@@ -1,37 +1,8 @@
 # Play backgammon
-from random import randint
 
-from board import Board
-from colour import Colour
+from game import Game
 
-
-def make_move(board, colour):
-    dice_roll = [randint(1, 6), randint(1, 6)]
-    if dice_roll[0] == dice_roll[1]:
-        dice_roll = [dice_roll[0]] * 4
-    print("%s rolled %s" % (colour, dice_roll))
-    for die_roll in dice_roll:
-        valid_pieces = board.get_pieces(colour)
-        for piece in valid_pieces:
-            if board.is_move_possible(piece, die_roll):
-                board.move_piece(piece, die_roll)
-                break
-
-
-i = randint(0, 1)
-print('%s goes first' % Colour(i))
-
-board = Board.create_starting_board()
-board.print_board()
-
-while True:
-    if i % 2 == 0:
-        make_move(board, Colour.WHITE)
-    else:
-        make_move(board, Colour.BLACK)
-    board.print_board()
-    i = i + 1
-    if board.has_game_ended():
-        print('%s has won!' % board.who_won())
-        break
-
+game = Game()
+game.run_game(verbose=False)
+winner = game.who_won()
+print(winner)
