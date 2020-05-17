9d21caa25e520457852fca7fc8e8324cfb02bf25
Anita Nandi <anita.k.nandi@gmail.com>
2020-05-17 22:36:57 +0100

    Allows using second dice without being able to use first, and doing fewer moves if not all dice can be used. Speed up by only trying one piece at each location

--- compare_all_moves_strategy.py
+++ compare_all_moves_strategy.py
@@ -28,17 +28,32 @@ class CompareAllMoves(Strategy):
     def move(self, board, colour, dice_roll):
 
         result = self.move_recursively(board, colour, dice_roll)
+        if len(result['pieces_to_try_swapped']) > 0:
+            new_dice_roll = dice_roll.copy()
+            new_dice_roll.reverse()
+            result_swapped = self.move_recursively(board, colour,
+                                                   dice_rolls=new_dice_roll,
+                                                   pieces_to_try=result['pieces_to_try_swapped'])
+            if result_swapped['best_value'] < result['best_value']:
+                result = result_swapped
 
         if result['best_moves'] is not None:
-            for i in range(len(result['best_moves'])):
-                board.move_piece(board.get_piece_at(result['best_moves'][i]), dice_roll[i])
+            for move in result['best_moves']:
+                board.move_piece(board.get_piece_at(move['piece_at']), move['die_roll'])
 
-
-    def move_recursively(self, board, colour, dice_rolls):
+    def move_recursively(self, board, colour, dice_rolls, pieces_to_try=None):
         best_board_value = float('inf')
         best_pieces_to_move = None
+        pieces_to_try_swapped = []
 
-        valid_pieces = board.get_pieces(colour)
+        if pieces_to_try is None:
+            pieces_to_try = [x.location for x in board.get_pieces(colour)]
+
+        pieces_to_try = list(set(pieces_to_try))
+
+        valid_pieces = []
+        for piece_location in pieces_to_try:
+            valid_pieces.append(board.get_piece_at(piece_location))
         valid_pieces.sort(key=Piece.spaces_to_home, reverse=True)
 
         dice_rolls_left = dice_rolls.copy()
@@ -49,18 +64,28 @@ class CompareAllMoves(Strategy):
                 board_copy = copy.deepcopy(board)
                 new_piece = board_copy.get_piece_at(piece.location)
                 board_copy.move_piece(new_piece, die_roll)
-                valid_pieces_new = board_copy.get_pieces(colour)
-                if len(valid_pieces_new) > 0 and len(dice_rolls_left) > 0:
+                if len(dice_rolls_left) > 0:
                     result = self.move_recursively(board_copy, colour, dice_rolls_left)
+                    if result['best_moves'] is None:
+                        # we have done the best we can do
+                        board_value = evaluate_board(board_copy, colour)
+                        if board_value < best_board_value:
+                            # Problem here: if it gives a better value to make less moves, this will be done.
+                            best_board_value = board_value
+                            best_pieces_to_move = [{'die_roll': die_roll, 'piece_at': piece.location}]
                     if result['best_value'] < best_board_value:
                         best_board_value = result['best_value']
-                        best_pieces_to_move = [piece.location] + result['best_moves']
+                        move = {'die_roll': die_roll, 'piece_at': piece.location}
+                        best_pieces_to_move = [move] + result['best_moves']
                 else:
                     board_value = evaluate_board(board_copy, colour)
                     if board_value < best_board_value:
                         best_board_value = board_value
-                        best_pieces_to_move = [piece.location]
+                        best_pieces_to_move = [{'die_roll': die_roll, 'piece_at': piece.location}]
+            elif len(dice_rolls_left) != 0 and (die_roll != dice_rolls_left[0]):
+                pieces_to_try_swapped.append(piece.location)
 
-
-        return {'best_value': best_board_value, 'best_moves': best_pieces_to_move}
+        return {'best_value': best_board_value,
+                'best_moves': best_pieces_to_move,
+                'pieces_to_try_swapped': pieces_to_try_swapped}
 
--- compare_all_moves_strategy_test.py
+++ compare_all_moves_strategy_test.py
@@ -26,6 +26,27 @@ class TestBoardIsMovePossible(TestBoardBase):
 
         self.assert_location(23, Contains(0).pieces())
 
+    def test_can_use_second_roll_without_first(self):
+        self.add_many_pieces(2, Colour.WHITE, 0)
+        self.add_many_pieces(2, Colour.BLACK, 3)
+
+        strategy = CompareAllMoves()
+        strategy.move(self.board, Colour.WHITE, [3, 4])
+
+        self.assert_location(4, Contains(1).pieces())
+        self.assert_location(0, Contains(1).pieces())
+
+    def test_can_use_not_all_dice(self):
+        self.add_many_pieces(3, Colour.WHITE, 18)
+        self.add_many_pieces(1, Colour.WHITE, 12)
+        self.add_many_pieces(2, Colour.BLACK, 17)
+
+        strategy = CompareAllMoves()
+        strategy.move(self.board, Colour.WHITE, [5, 5, 5, 5])
+
+        self.assert_location(23, Contains(3).pieces())
+        self.assert_location(12, Contains(1).pieces())
+
 
 if __name__ == '__main__':
     unittest.main()
