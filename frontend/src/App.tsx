daae4fd1c01be3e36c98a463f4c934fcc34e7550
Anita Nandi <anita.k.nandi@gmail.com>
2020-05-30 23:59:51 +0100

    Add images for pieces

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import backgammon_board from './backgammon_board.gif';
+import backgammonBoard from './backgammon-board.gif';
+import whitePiece from './white-piece.svg'
+import blackPiece from './black-piece.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={backgammon_board} className="board" alt="logo" />
+        <img src={backgammonBoard} className="board" alt="logo" />
+        <img src={whitePiece} className="board" alt="logo" />
+        <img src={blackPiece} className="board" alt="logo" />
       </header>
     </div>
   );
